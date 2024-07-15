b00bd4a13f7bfafc5650695d8b19e0f747be74d6
L-I-V-I-O <livio.kuerzi@stud.msw.ch>
2024-07-15 16:29:13 +0200

    Projekt aufsetzen

---
+++ Reaction-Game/Reaction-Game/main.c
@@ -0,0 +1,18 @@
+/*
+ * Reaction-Game.c
+ *
+ * Created: 15.07.2024 16:26:43
+ * Author : e2Kuerzi
+ */ 
+
+#include "ucBoardDriver.h"
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
